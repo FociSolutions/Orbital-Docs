9e731042e3a05468fc2c6f515379934c7aab6c66
glitch452 <894128+glitch452@users.noreply.github.com>
2022-05-03 15:12:40 -0700

    Updated JS and rewrote css for copy button on inline and fenced code blocks

--- assets/js/copy-to-clipboard.js
+++ assets/js/copy-to-clipboard.js
@@ -2,28 +2,47 @@
  * Adapted from https://github.com/marcoaugustoandrade/jekyll-clipboardjs
  */
 
-document.addEventListener("DOMContentLoaded", function (event) {
-  let codes = document.querySelectorAll("code");
+document.addEventListener('DOMContentLoaded', () => {
+  if (!ClipboardJS.isSupported()) {
+    return;
+  }
+
+  const codes = document.querySelectorAll('code');
   let countID = 0;
 
   codes.forEach((code) => {
-    console.log("Located code block", code);
+    const minTextLengthToAddCopyButton = 3;
+    if (code.innerText.length < minTextLengthToAddCopyButton) {
+      return;
+    }
 
-    code.setAttribute("id", "code" + countID);
+    const id = `code-${countID}`;
+    code.setAttribute('id', id);
 
-    let btn = document.createElement("button");
-    btn.innerHTML = "Copy";
-    btn.className = "btn-copy";
-    btn.setAttribute("data-clipboard-action", "copy");
-    btn.setAttribute("data-clipboard-target", "#code" + countID);
+    const icon = document.createElement('i');
+    icon.className = 'fas fa-copy';
 
-    let div = document.createElement("div");
-    div.appendChild(btn);
+    const container = document.createElement('span');
+    container.className = 'btn-copy';
+    container.setAttribute('data-clipboard-action', 'copy');
+    container.setAttribute('data-clipboard-target', `#${id}`);
+    container.setAttribute('title', 'Copy to Clipboard');
+    container.appendChild(icon);
 
-    code.before(div);
+    const isFencedCodeBlock = code.parentElement.classList.contains('highlight');
+
+    if (isFencedCodeBlock) {
+      const text = document.createElement('span');
+      text.innerText = 'Copy';
+      container.appendChild(text);
+
+      code.before(container);
+    } else {
+      code.after(container);
+    }
 
     countID++;
   });
 
-  new ClipboardJS(".btn-copy");
+  new ClipboardJS('.btn-copy');
 });
